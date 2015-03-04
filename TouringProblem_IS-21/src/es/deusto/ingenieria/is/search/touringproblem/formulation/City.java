63707e0fda9a37fea6563db80564b6aaf1d84c96
Adritxan <adriana.prado@opendeusto.es>
2015-03-04 12:16:44 +0100

    fsds

---
+++ TouringProblem_IS-21/src/es/deusto/ingenieria/is/search/touringproblem/formulation/City.java
@@ -0,0 +1,50 @@
+package es.deusto.ingenieria.is.search.touringproblem.formulation;
+
+/*
+ *  Clase Ciudad con los atributos nombre y la posicion mediante x e y
+ */
+public class City {
+	
+	private String name;
+	private int x;
+	private int y;
+	//private List<City> ciudadesAccesibles = new ArrayList<City>();
+	
+	
+	public City(String name, int x, int y) {
+		super();
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+	
+	/*
+	 * Getters and setters de los atributos de la clase
+	 */
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	/*
+	 * Método toString que devuelve un String con los datos de la ciudad 
+	 */
+	public String toString(){
+		return "City:"+name+" 	 x:"+x+" 	y:"+y;
+	}
+
+}
